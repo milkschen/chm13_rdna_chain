d6d90d37a1e055d365f684038797d433adffe9e3
milkschen <s93rm6@gmail.com>
2021-09-22 20:11:48 -0400

    supports the updated pipeline

--- rdna_paf_to_chain.py
+++ rdna_paf_to_chain.py
@@ -18,8 +18,32 @@ NRES = 9
 ALN = 10
 MAPQ = 11
 
-rdna_map = {'rdna13': 'chr13', 'rdna14': 'chr14', 'rdna15': 'chr15',
-            'rdna21': 'chr21', 'rdna22': 'chr22'}
+# original naming system
+# rdna_map = {'rdna13': 'chr13', 'rdna14': 'chr14', 'rdna15': 'chr15',
+#             'rdna21': 'chr21', 'rdna22': 'chr22'}
+
+# 1.0->1.1
+rdna_map = {'chr13:5650893-5650893': 'chr13',
+            'chr13:10020808-10061802': 'chr13',
+            'chr14:1981995-2100015': 'chr14',
+            'chr14:2875496-2918014': 'chr14',
+            'chr15:2388406-2506438': 'chr15',
+            'chr15:5292607-5333560': 'chr15',
+            'chr21:2988589-3108297': 'chr21',
+            'chr21:6349728-6389624': 'chr21',
+            'chr22:4674015-4793755': 'chr22',
+            'chr22:5749418-5788909': 'chr22',
+# 1.0 -> 1.0
+            'chr13:5650823-5774302': 'chr13',
+            'chr13:9341786-9389035': 'chr13',
+            'chr14:1981517-2103527': 'chr14',
+            'chr14:2811590-2860329': 'chr14',
+            'chr15:2388410-2510165': 'chr15',
+            'chr15:4701196-4748438': 'chr15',
+            'chr21:2988590-3112035': 'chr21',
+            'chr21:5608487-5652611': 'chr21',
+            'chr22:4674054-4797629': 'chr22',
+            'chr22:5714337-5760141': 'chr22'}
 
 def parse_args():
     # START_CHAIN_IDX = 25
@@ -30,7 +54,7 @@ def parse_args():
         help='Path to the PAF file where rDNA patches are aligned to an unpolished reference.'
     )
     parser.add_argument(
-        '-ic', '--start_chain_idx', default=25,
+        '-ic', '--start_chain_idx', default=25, type=int,
         help='Starting index of the output chain records.'
     )
     parser.add_argument(
@@ -64,7 +88,8 @@ def rdna_paf_to_chain(args):
     paf.sort(key=lambda x: int(x[NRES]), reverse=True)
     paf_final = []
     for p in paf:
-        if rdna_map[p[QNAME]] != p[TNAME]:
+        if p[QNAME].split(':')[0] != p[TNAME]:
+        # if rdna_map[p[QNAME]] != p[TNAME]:
             continue
         s = ''
         for i in paf_fields:
@@ -115,7 +140,8 @@ def rdna_paf_to_chain(args):
         for r in f:
             if not r.is_supplementary and not r.is_secondary:
                 if r.query_name not in rdna_offsets:
-                    if r.reference_name == rdna_map[r.query_name]:
+                    if r.query_name.split(':')[0] == r.reference_name:
+                    # if r.reference_name == rdna_map[r.query_name]:
                         rdna_offsets[r.query_name] = r.pos
                         rdna_chrlen[r.reference_name] = f.get_reference_length(r.reference_name)
 
@@ -133,11 +159,13 @@ def rdna_paf_to_chain(args):
     re_cigar = re.compile('[MIDS]+')
     for idx_p, p in enumerate(paf_final):
         qname = p[QNAME]
-        chr_qname = rdna_map[qname]
+        # chr_qname = rdna_map[qname]
+        chr_qname = qname.split(':')[0]
         qstart = int(p[QSTART]) + rdna_offsets[qname]
         qend = int(p[QEND]) + rdna_offsets[qname]
-        print((f'chain {p[NRES]} {p[TNAME]} {p[TLEN]} + {p[TSTART]} {p[TEND]} '
+        print((f'chain {p[NRES]} '
                f'{chr_qname} {rdna_chrlen[chr_qname]} {p[STRAND]} {qstart} {qend} '
+               f'{p[TNAME]} {p[TLEN]} + {p[TSTART]} {p[TEND]} '
                f'{args.start_chain_idx+idx_p}'), file=f_out)
 
         cg = paf_retrieve_cigar(p)
@@ -146,16 +174,14 @@ def rdna_paf_to_chain(args):
         match = None
         dt = None
         dq = None
-        # print(cg)
         for i_cg, op in enumerate(cg_ops):
-            # print(op, cg_lens[i_cg])
             if op == 'M':
                 match = cg_lens[i_cg]
-            elif op == 'I':
+            elif op == 'D':
                 assert match is not None
                 print(f'{match} 0 {cg_lens[i_cg]}', file=f_out)
                 match = None
-            elif op == 'D':
+            elif op == 'I':
                 assert match is not None
                 print(f'{match} {cg_lens[i_cg]} 0', file=f_out)
                 match = None
