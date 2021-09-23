a4f59f07c621d5f06515a89b0185535464e158cc
milkschen <s93rm6@gmail.com>
2021-09-22 20:41:14 -0400

    updated

--- rdna_to_chain.sh
+++ rdna_to_chain.sh
@@ -14,6 +14,12 @@ set -x
 #      from flanking/v1.0.list
 #  - v1.1.list
 #      rDNA flanking sequence regions in CHM13 v1.1
+#      from flanking/v1.1.list
+
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.v1.0.fasta
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.v1.0.fasta.fai
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.v1.1.fasta
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.v1.1.fasta.fai
 
 # Extract flanking sequences
 samtools faidx -r flanking/v1.0.list chm13.v1.0.fasta > chm13.v1.0.rDNA_flanking.fa
@@ -31,13 +37,13 @@ winnowmap -k 19 -a -W repetitive_k19_v1.1.txt chm13.v1.1.fasta chm13.v1.1.rDNA_f
 winnowmap -k 19 -c -W repetitive_k19_v1.1.txt chm13.v1.1.fasta chm13.v1.0.rDNA_flanking.fa > chm13.v1.0.rDNA_flanking.to_v1.1.paf
 
 # Build chain from 1.1 to 1.0
-wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/changes/v1.0_to_v1.1/v1.1_to_v1.0.chain
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/changes/v1.0_to_v1.1/v1.1_to_v1.0.chain
 python rdna_paf_to_chain.py -p chm13.v1.1.rDNA_flanking.to_v1.0.paf  -a chm13.v1.1.rDNA_flanking.to_v1.1.sam  -o chm13.v1.1.rDNA_flanking.to_v1.0.chain -ic 25
 cp v1.1_to_v1.0.chain v1.1_to_v1.0_rdna.chain
 cat chm13.v1.1.rDNA_flanking.to_v1.0.chain >> v1.1_to_v1.0_rdna.chain
 
 # Build chain from 1.0 to 1.1
-wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/changes/v1.0_to_v1.1/v1.0_to_v1.1.chain
+wget -nc https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/changes/v1.0_to_v1.1/v1.0_to_v1.1.chain
 python rdna_paf_to_chain.py -p chm13.v1.0.rDNA_flanking.to_v1.1.paf -a chm13.v1.0.rDNA_flanking.to_v1.0.sam -o chm13.v1.0.rDNA_flanking.to_v1.1.chain -ic 25
 cp v1.0_to_v1.1.chain v1.0_to_v1.1_rdna.chain
 cat chm13.v1.0.rDNA_flanking.to_v1.1.chain >> v1.0_to_v1.1_rdna.chain
